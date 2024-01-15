d486049d9c0212284ecb0ae90337c8b457e9903d
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-01-15 13:44:30 +0000

    update

---
+++ src/reticolo.hpp
@@ -0,0 +1,19 @@
+// LQGR.hpp
+
+// Main include file for the entire LQGR stuff
+
+#pragma once
+
+#include <config.h>
+
+#include "tools/timer.hpp"
+#include "tools/types.hpp"
+#include "tools/io.hpp"
+
+#include "action/action_base.hpp"
+#include "action/phi4.hpp"
+
+#include "lattice/lattice.hpp"
+
+#include "llr/llr_worker.hpp"
+#include "llr/llr_controller.hpp"
