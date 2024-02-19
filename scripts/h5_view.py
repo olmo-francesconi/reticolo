55f455d85c09ce2de76163a0c6cd47831ba6f0b9
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-02-19 19:24:31 +0000

    random stuff

--- scripts/h5_view.py
+++ scripts/h5_view.py
@@ -5,22 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-f = h5py.File("../build/4_4_4_4/meas/llr.h5", "r")
+f = h5py.File("../build/4_4_4_4/llr/meas/llr.h5", "r")
 
-runs = f.keys()
-# aks = f[runs[0]].keys()
-print(runs[1])
-# print(aks)
 
-print(np.array(f["run0/ak_001"]))
+for i in range(10):
+    for j in range(12):
+        data = np.array(f["run{0}/ak_{1:0>3d}".format(i, j)])
+        plt.plot(data)
 
-# for r in runs:
-#     ak = f[r].keys()
-#     # data = []
 
-#     for a in ak:
-#         tmp = pd.DataFrame(f[r][a]).to_numpy()
-#         print(pd.DataFrame(f[r][a]))
-#         # np.concatenate((data, tmp), axis=0)
-
-#     # print(data)
+plt.show()
