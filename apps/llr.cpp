2aec9e2e8686149ecb867d1e47205f12f1875aec
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-02-20 17:28:04 +0000

    Complete LLR workflow example

--- apps/llr.cpp
+++ apps/llr.cpp
@@ -1,44 +1,56 @@
-#include <cstdlib>
-#include <format>
-#include <string>
-#include <vector>
+/******************************************************************************
 
-#include "reticolo/action/phi4.hpp"
+ - reticolo (www.github.com/olmo-francesconi/reticolo.git)
+
+ - SourceFile: apps/llr.hpp
+
+ - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
+
+ ******************************************************************************/
+
+#include <cstdlib>
+#include <string>
+
 #include "reticolo/reticolo.hpp"
+#include "reticolo/types/core.hpp"
 
 using namespace reticolo;
 
 auto main(int argc, char* argv[]) -> int {
-    std::vector<uintvect<4>> Volumes = {
-        {4, 4, 4, 4},
-        // {6, 6, 6, 6},
-        // {8, 8, 8, 8},
-        // {10, 10, 10, 10},
-        // {12, 12, 12, 12},
-        // {14, 14, 14, 14},
-        // {16, 16, 16, 16},
-        // {64, 64, 64, 64},
-    };
+    /* Define the lattice volume */
+    uintvect<4> Volume = {4, 4, 4, 4};
 
-    for (const auto& Volume : Volumes) {
-        // std::cout << std::format("Lattice size: {} x {} x {} x {}", volume[_t],
-        // volume[_x], volume[_y], volume[_z]) << std::endl;
+    /*  Set the output folder to be "./nt_nx_ny_nz/"
+        LLR workspace folder structure:
+            OutPath
+            ├── llr
+            │   ├── logs
+            │   │   ├── llr_worker[000].log
+            │   │   ├── ...
+            │   │   └── llr_worker[###].log
+            │   └── meas
+            │       ├── llr.h5
+            │       ├── llr_worker[000].h5
+            │       ├── ...
+            │       └── llr_worker[###].h5
+            └── reticolo.log                         */
+    std::string OutPath = "BoseGasLLR";
 
-        for (int DMuI = 0; DMuI < 10; DMuI++) {
-            double Mu = 0.1 * DMuI;
+    /* Initialize the action */
+    action::BoseGas<ComplexD, ComplexD> Action(1.0, 9.0, 1.0);
 
-            action::BoseGas<ComplexD, ComplexD> Action(1.0, 9.0, Mu);
+    /* Declare the LLRController
+        This will take care of setting up and running the simulation
+        Spawn al the LLRWorkers and control the multi-threaded operation */
+    LLR::LLRController Cont(Action);
 
-            LLR::LLRController Cont(Action);
+    /* Initialize the LLRController
+        This will actually allocate memory and set up all the workers */
+    Cont.init(OutPath, Volume, 1, 0.0025);
 
-            std::string OutPath =
-                std::format("./{}_{}_{}_{}/{:3.2}", Volume[_t], Volume[_x], Volume[_y], Volume[_z], Mu);
-
-            Cont.init(OutPath, Volume, 1, 0.0025, LOG_mode::silent);
-
-            Cont.run(50, 50, 1000, "run", 1);
-        }
-    }
+    /* Run the LLR simulation
+        This will perform nMonteCarlo updates for each NR or RM step */
+    Cont.run(50, 50, 1000, "BoseGasLLR", 2);
 
     return EXIT_SUCCESS;
 }
