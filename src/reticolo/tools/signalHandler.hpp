ff2fb96d21d0757ea1c158a2b60cb020b3d3ebe2
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-02-21 17:05:26 +0000

    initial commit

---
+++ src/reticolo/tools/signalHandler.hpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+ - reticolo (www.github.com/olmo-francesconi/reticolo.git)
+
+ - SourceFile: tools/sig_handler.hpp
+
+ - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
+
+******************************************************************************/
+
+#include <csignal>
+#include <cstdlib>
+#include <iostream>
+
+#include "reticolo/tools/io_utils.hpp"
+
+using Handler = void (*)(int);
+
+namespace reticolo::SignalHandler {
+
+/* SIGINT soft-exit handler */
+inline bool SoftexitRequested = false;
+inline void SIGINT_SoftExit(int signum) {
+    if (!SoftexitRequested) {
+        std::cout << '\n' << IO::LI_warn() << "SoftExit requested (Ctrl+C again to exit immediately)\n";
+    } else {
+        std::cout << '\n' << IO::LI_erro() << "Immediate exit requested, possible data corruption.\n";
+        std::cout << IO::LI_erro() << "Bye :(\n";
+
+        exit(SIGINT);
+    }
+    SoftexitRequested = true;
+}
+
+/* Install new handler */
+inline void set_SIGINT_handler(void (*new_hadler)(int)) { signal(SIGINT, new_hadler); }
+
+/* Restore original handler */
+inline void SIGINT_reset() { signal(SIGINT, SIG_DFL); }
+
+}  // namespace reticolo::SignalHandler
