d443bd7ee2ddebe0a7871e2379dc345c4ded541a
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-02-27 10:46:39 +0000

    boh

--- scripts/MetropolisMonteCarlo_h5_view.py
+++ scripts/MetropolisMonteCarlo_h5_view.py
@@ -5,16 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-f = h5py.File(
-    "/Users/olmo/software/source/reticolo/build/apps/MetropolisMonteCarlo_out/meas/MetropolisMonteCarlo.h5", "r"
-)
-data = pd.DataFrame(np.array(f["MetropolisMonteCarlo/MonteCarlo"]))
+f = h5py.File("/Users/olmo/software/source/reticolo/build/apps/WeakFieldEuclideanGR/meas/WeakFieldEuclideanGR.h5", "r")
+data = pd.DataFrame(np.array(f["WeakFieldEuclideanGR/MonteCarlo"]))
 
-autocorr = []
-for t in range(1, 100):
-    autocorr.append(data["S_re"].autocorr(t))
+# autocorr = []
+# for t in range(1, 100):
+#     autocorr.append(data["S_re"].autocorr(t))
 
-plt.plot(autocorr)
-plt.show()
-data.plot()
+# plt.plot(autocorr)
+# plt.show()
+data["acceptance"].plot()
+
 plt.show()
