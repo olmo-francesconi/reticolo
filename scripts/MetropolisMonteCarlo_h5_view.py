51e241d2c6b26eccd3c8e5ee2122196924169df5
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-03-05 15:33:05 +0000

    various changes

--- scripts/MetropolisMonteCarlo_h5_view.py
+++ scripts/MetropolisMonteCarlo_h5_view.py
@@ -5,15 +5,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-f = h5py.File("/Users/olmo/software/source/reticolo/build/apps/WeakFieldEuclideanGR/meas/WeakFieldEuclideanGR.h5", "r")
-data = pd.DataFrame(np.array(f["WeakFieldEuclideanGR/MonteCarlo"]))
+steps = np.arange(10, 101, 10)
+stepsizes = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1]
+path = "/Users/olmo/software/source/reticolo/build/apps/test/meas/"
 
-# autocorr = []
-# for t in range(1, 100):
-#     autocorr.append(data["S_re"].autocorr(t))
+for step in steps:
+    for stepsize in stepsizes:
+        name = "HMC_{}_{}".format(step, stepsize)
+        print(name)
+        f = h5py.File(path + name + ".h5", "r")
 
-# plt.plot(autocorr)
-# plt.show()
-data["acceptance"].plot()
+        data = pd.DataFrame(np.array(f[name + "/MonteCarlo"]))
 
-plt.show()
+        autocorr = []
+        for t in range(1, 200):
+            autocorr.append(data["S_re"][10000:].autocorr(t))
+
+        plt.plot(autocorr, label=name)
+
+    plt.legend(loc=0)
+    plt.show()
