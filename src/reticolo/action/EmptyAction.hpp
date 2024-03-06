b51ffa025c67054bef37be7b94eab6d51487db65
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-03-06 10:49:52 +0000

    added dimension to template arguments

--- src/reticolo/action/EmptyAction.hpp
+++ src/reticolo/action/EmptyAction.hpp
@@ -12,6 +12,7 @@
 
 #pragma once
 
+#include <cstddef>
 #include <format>
 #include <string>
 #include <vector>
@@ -27,18 +28,19 @@ namespace reticolo::action {
 /*--------------------------------------------------------------------------------------------------
   EmptyAction Class Declaration
 --------------------------------------------------------------------------------------------------*/
-template <ComplexValue TField, ComplexValue TAction>
-class EmptyAction : ActionBase<TField, TAction, 4> {
+template <ComplexValue TField, ComplexValue TAction, size_t dim>
+class EmptyAction : ActionBase<TField, TAction, dim> {
   public:
     /* Types and public action metadata */
     using FieldType = TField;      // Type of the field variables
     using ActionType = TAction;    // Return type fo the action
-    const static int Dims = 4;     // Dimensions of the action
+    const static int Dims = dim;   // Dimensions of the action
     const static int Stencil = 2;  // Minimum step size for multi-thread safety
 
     /* Algorithm capabilities */
-    const static bool IsMetropolisCapable = true;
-    const static bool IsHmcCapable = true;
+    const static bool IsMetropolisCapable = false;
+    const static bool IsHmcCapable = false;
+    const static bool IsLLRCapable = false;
 
     /* Action parameters */
     struct Params {
@@ -59,10 +61,7 @@ class EmptyAction : ActionBase<TField, TAction, 4> {
         return Type;
     }
 
-    /* Constructors */
-    EmptyAction() = default;  // Default
-
-    /* Initializer Construtors */
+    /* Construtors */
     EmptyAction(double param) : p(param){};    // Parameter List
     EmptyAction(Params params) : p(params){};  // Parameter struct
 
@@ -70,18 +69,18 @@ class EmptyAction : ActionBase<TField, TAction, 4> {
     ~EmptyAction() = default;
 
     /* Sync with lattice */
-    void lattice_sync(const Lattice<FieldType, 4>& field) override;
+    void lattice_sync(const Lattice<FieldType, dim>& field) override;
 
     /* Gloabal and local action computations */
-    auto compute_S(const Lattice<FieldType, 4>& field) -> ActionType override;
-    auto compute_S_loc(const Lattice<FieldType, 4>& field, uint site) -> ActionType override;
-    auto compute_dS_loc(const Lattice<TField, 4>& field, const TField& dphi, uint site) -> ActionType override;
+    auto compute_S(const Lattice<FieldType, dim>& field) -> ActionType override;
+    auto compute_S_loc(const Lattice<FieldType, dim>& field, uint site) -> ActionType override;
+    auto compute_dS_loc(const Lattice<TField, dim>& field, const TField& dphi, uint site) -> ActionType override;
 
     /* HMC methods */
-    void compute_Forces(const Lattice<FieldType, 4>& field, Lattice<FieldType, 4> Forces) override;
+    void compute_Forces(const Lattice<FieldType, dim>& field, Lattice<FieldType, dim> Forces) override;
 
     /* Perform the measurements or returns updated Observable values*/
-    static auto Measure(const Lattice<FieldType, 4>& field) -> Observables;
+    static auto Measure(const Lattice<FieldType, dim>& field) -> Observables;
 
     /* Log stuff */
     auto action_name() -> std::string override { return "EmptyAction"; };
@@ -94,20 +93,20 @@ class EmptyAction : ActionBase<TField, TAction, 4> {
 /*--------------------------------------------------------------------------------------------------
   Public methods Implementatin
 --------------------------------------------------------------------------------------------------*/
-template <ComplexValue TField, ComplexValue TAction>
-inline void EmptyAction<TField, TAction>::lattice_sync(const Lattice<FieldType, 4>& field) {}
+template <ComplexValue TField, ComplexValue TAction, size_t dim>
+inline void EmptyAction<TField, TAction, dim>::lattice_sync(const Lattice<TField, dim>& field) {}
 
-template <ComplexValue TField, ComplexValue TAction>
-inline auto EmptyAction<TField, TAction>::compute_S(const Lattice<TField, 4>& Field) -> TAction {}
+template <ComplexValue TField, ComplexValue TAction, size_t dim>
+inline auto EmptyAction<TField, TAction, dim>::compute_S(const Lattice<TField, dim>& Field) -> TAction {}
 
-template <ComplexValue TField, ComplexValue TAction>
-inline auto EmptyAction<TField, TAction>::compute_S_loc(const Lattice<TField, 4>& Field, uint Site) -> TAction {}
+template <ComplexValue TField, ComplexValue TAction, size_t dim>
+inline auto EmptyAction<TField, TAction, dim>::compute_S_loc(const Lattice<TField, dim>& Field, uint Site) -> TAction {}
 
-template <ComplexValue TField, ComplexValue TAction>
-inline auto EmptyAction<TField, TAction>::compute_dS_loc(const Lattice<TField, 4>& Field, const TField& dPhi, uint Site)
-    -> TAction {}
+template <ComplexValue TField, ComplexValue TAction, size_t dim>
+inline auto EmptyAction<TField, TAction, dim>::compute_dS_loc(const Lattice<TField, dim>& Field, const TField& dPhi,
+                                                              uint Site) -> TAction {}
 
-template <ComplexValue TField, ComplexValue TAction>
-inline auto EmptyAction<TField, TAction>::Measure(const Lattice<TField, 4>& field) -> EmptyAction::Observables {}
+template <ComplexValue TField, ComplexValue TAction, size_t dim>
+inline auto EmptyAction<TField, TAction, dim>::Measure(const Lattice<TField, dim>& field) -> EmptyAction::Observables {}
 
 }  // namespace reticolo::action
