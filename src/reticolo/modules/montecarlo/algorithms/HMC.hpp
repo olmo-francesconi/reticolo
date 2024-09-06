95411fc672bb747ef97db8b5d8db191e1e13884a
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-06 11:33:45 +0100

    Updated MonteCarlo algorithms

--- src/reticolo/modules/montecarlo/algorithms/HMC.hpp
+++ src/reticolo/modules/montecarlo/algorithms/HMC.hpp
@@ -12,58 +12,68 @@
 
 #include <cstdlib>
 #include <memory>
+#include <random>
 
 #include "reticolo/lattice/Lattice.hpp"
+#include "reticolo/modules/factory/MCAlgorithmBase.hpp"
 #include "reticolo/modules/montecarlo/MonteCarloData.hpp"
-#include "reticolo/modules/montecarlo/algorithms/AlgorithmBase.hpp"
 #include "reticolo/types/core.hpp"
 #include "reticolo/types/core_math.hpp"
 #include "reticolo/types/random.hpp"
 #include "yaml-cpp/node/node.h"
 
-namespace reticolo::montecarlo {
+namespace reticolo::MMonteCarlo {
 
 template <class Action>
-class HMC : public AlgorithmBase<Action> {
+class HMC : public MCAlgorithmBase<Action> {
   private:
     /* Types definitions */
-    using ActionType = typename Action::ActionType;
-    using FieldType = typename Action::FieldType;
-    using LatticeType = Lattice<typename Action::FieldType>;
-    using monte_carlo_data_type = montecarlo::data<typename Action::ActionType>;
+    using action_type = typename Action::ActionType;
+    using field_type = typename Action::FieldType;
+    using lattice_type = Lattice<field_type>;
+    using monte_carlo_data_type = MMonteCarlo::data<action_type>;
 
     /* HMC support fields */
-    std::unique_ptr<LatticeType> _Mom;
-    std::unique_ptr<LatticeType> _Forces;
-    std::unique_ptr<LatticeType> _OldField;
+    std::unique_ptr<lattice_type> _Mom;
+    std::unique_ptr<lattice_type> _Forces;
+    std::unique_ptr<lattice_type> _OldField;
 
     /* HMC settings */
-    double _Stepsize;
-    uint   _Steps;
+    RealD _Stepsize;
+    uint  _Steps;
+
+    /* Distributions */
+    std::uniform_real_distribution<RealD> _Unif;  // Uniform distribution [0.0, 1.0]
+    std::normal_distribution<RealD>       _Norm;  // Normal distibution (mean: 0.0, stddev: 1.0)
 
   public:
     HMC() = default;
 
     /* setup */
-    void setup(const YAML::Node& HMCParams, const Lattice<FieldType>& Field) override {
+    void setup(const YAML::Node& Params, const Lattice<field_type>& Field) override {
         /* Parse Parameters */
-        _Stepsize = HMCParams["stepsize"].as<double>();
-        _Steps = HMCParams["steps"].as<uint>();
+        _Stepsize = Params["stepsize"].as<RealD>();
+        _Steps = Params["steps"].as<uint>();
 
-        _Mom = std::make_unique<Lattice<FieldType>>(Field);
-        _Forces = std::make_unique<Lattice<FieldType>>(Field);
-        _OldField = std::make_unique<Lattice<FieldType>>(Field);
+        /* Allocate auxiliary fields */
+        _Mom = std::make_unique<Lattice<field_type>>(Field);
+        _Forces = std::make_unique<Lattice<field_type>>(Field);
+        _OldField = std::make_unique<Lattice<field_type>>(Field);
+
+        /* Initialize distributions */
+        _Unif = std::uniform_real_distribution<RealD>(0.0, 1.0);
+        _Norm = std::normal_distribution<RealD>(0.0, 1.0);
     }
 
     /* execution */
-    void updateField(Lattice<FieldType>& field, Action& action, monte_carlo_data_type& state) override {
+    void updateField(Lattice<field_type>& field, Action& action, monte_carlo_data_type& state, RNGType& rng) override {
         int NSites = field.getNsites();
         // save the old field configuration;
         *_OldField = field;
         // Generate random momenta and compute initial kinetic term
-        RealD OldK(0.0);
+        RealD OldK = 0.0;
         for (int Site = 0; Site < NSites; Site++) {
-            // randomize(_Mom[Site], 1.0, _Norm, _Rng);
+            randomize((*_Mom)[Site], 1.0, _Norm, rng);
             OldK += dot((*_Mom)[Site]);
         }
         OldK *= 0.5;
@@ -91,147 +101,23 @@ class HMC : public AlgorithmBase<Action> {
             (*_Mom)[Site] += 0.5 * _Stepsize * (*_Forces)[Site];
         }
         // Compute final action
-        ActionType OldS = state.getS();
-        ActionType NewS = action.compute_S(field);
+        action_type OldS = state.getS();
+        action_type NewS = action.compute_S(field);
         // Compute end kinetic term
-        RealD NewK(0.0);
+        RealD NewK = 0.0;
         for (int Site = 0; Site < NSites; Site++) {
             NewK += dot((*_Mom)[Site]);
         }
         NewK *= 0.5;
 
         // Final Metropolis check
-        // if (exp(OldK - NewK + make_real(OldS - NewS)) > m_Unif(_Rng)) {
-        //     state.update(1, NewS - OldS);
-        // } else {
-        //     field = _OldField;
-        //     state.update(0, 0.0);
-        // }
+        if (exp(OldK - NewK + make_real(OldS - NewS)) > _Unif(rng)) {
+            state.update(1, NewS - OldS);
+        } else {
+            field = *_OldField;
+            state.update(0, 0.0);
+        }
     }
 };
 
-}  // namespace reticolo::montecarlo
-
-// template <HmcCapable Action>
-// class HMC : public MonteCarloHandler<Action> {
-//   private:
-//     /* Types definitions */
-//     using ActionType = typename Action::ActionType;
-//     using FieldType = typename Action::FieldType;
-//     using LatticeType = Lattice<typename Action::FieldType>;
-
-//     /* HMC support fields */
-//     LatticeType _Mom;
-//     LatticeType _Forces;
-//     LatticeType _OldField;
-
-//     /* HMC settings */
-//     double _Stepsize;
-//     uint   _Steps;
-
-//     /* Introduced names from MonteCarloHandler (avoids using this->...) */
-//     using MonteCarloHandler<Action>::_Field;
-//     using MonteCarloHandler<Action>::_Action;
-//     using MonteCarloHandler<Action>::_McStats;
-//     using MonteCarloHandler<Action>::_Unif;
-//     using MonteCarloHandler<Action>::_UnifC;
-//     using MonteCarloHandler<Action>::_Norm;
-//     using MonteCarloHandler<Action>::_Rng;
-//     using MonteCarloHandler<Action>::_Logger;
-//     using MonteCarloHandler<Action>::_Timer;
-
-//   public:
-//     /* Constructor */
-//     HMC(std::string handler_name, Action& action, LatticeType& field, uint seed, const std::string& output_path,
-//         bool StdOut, bool save_data, bool save_config);
-
-//     /* override virtual updateField() method */
-//     void updateField() override;
-
-//     /* Initialize parameters */
-//     void setParams(double traj_length, uint steps) {
-//         _Steps = steps;
-//         _Stepsize = traj_length / _Steps;
-//     }
-// };
-
-// template <HmcCapable Action>
-// HMC<Action>::HMC(std::string        handler_name,  //
-//                  Action&            action,        //
-//                  LatticeType&       field,         //
-//                  uint               seed,          //
-//                  const std::string& output_path,   //
-//                  bool               StdOut,        //
-//                  bool               save_data,     //
-//                  bool               save_config)
-//     : montecarlo::MonteCarloHandler<Action>(output_path,    //
-//                                             handler_name,   //
-//                                             action, field,  //
-//                                             seed,           //
-//                                             StdOut,         //
-//                                             save_data,      //
-//                                             save_config),
-//       _Mom(field.getSizes()),
-//       _Forces(field.getSizes()),
-//       _OldField(field.getSizes()) {
-//     // Log stuff
-//     _Logger << IO::LI_time() +
-//                    std::format("Monte Carlo Handler - Initialization completed in {:.3f} ms\n", _Timer.elapsed_ms());
-// }
-
-// template <HmcCapable Action>
-// void HMC<Action>::updateField() {
-//     int NSites = _Field.getNsites();
-//     // save the old field configuration;
-//     _OldField = _Field;
-//     // Generate random momenta and compute initial kinetic term
-//     RealD OldK(0.0);
-//     for (int Site = 0; Site < NSites; Site++) {
-//         randomize(_Mom[Site], 1.0, _Norm, _Rng);
-//         OldK += dot(_Mom[Site]);
-//     }
-//     OldK *= 0.5;
-//     // Compute Forces
-//     _Action.compute_Forces(_Field, _Forces);
-//     // Momenta half step
-//     for (int Site = 0; Site < NSites; Site++) {
-//         _Mom[Site] -= 0.5 * _Stepsize * _Forces[Site];
-//     }
-//     // Leapfrog algorithm
-//     for (uint Step = 0; Step < _Steps; Step++) {
-//         // Update field
-//         for (int Site = 0; Site < NSites; Site++) {
-//             _Field[Site] += _Stepsize * _Mom[Site];
-//         }
-//         // Compute updated forces
-//         _Action.compute_Forces(_Field, _Forces);
-//         // Update momenta
-//         for (int Site = 0; Site < NSites; Site++) {
-//             _Mom[Site] -= _Stepsize * _Forces[Site];
-//         }
-//     }
-//     // Momenta half step roll-back
-//     for (int Site = 0; Site < _Mom.getNsites(); Site++) {
-//         _Mom[Site] += 0.5 * _Stepsize * _Forces[Site];
-//     }
-//     // Compute final action
-//     ActionType NewS = _Action.compute_S(_Field);
-//     // Compute end kinetic term
-//     RealD NewK(0.0);
-//     for (int Site = 0; Site < _Mom.getNsites(); Site++) {
-//         NewK += dot(_Mom[Site]);
-//     }
-//     NewK *= 0.5;
-//     // Final Metropolis check
-//     if (exp(OldK - NewK + make_real(_McStats.getS() - NewS)) > m_Unif(_Rng)) {
-//         _McStats.update(1, NewS - _McStats.getS());
-//     } else {
-//         _Field = _OldField;
-//         _McStats.update(0, 0.0);
-//     }
-// }
-
-// /* Argument deduction guide */
-// template <HmcCapable Action>
-// HMC(std::string, Action&, Lattice<typename Action::FieldType>, uint, std::string&, bool, bool, bool) -> HMC<Action>;
-// }  // namespace reticolo::montecarlo
+}  // namespace reticolo::MMonteCarlo
--- src/reticolo/modules/montecarlo/algorithms/Metropolis.hpp
+++ src/reticolo/modules/montecarlo/algorithms/Metropolis.hpp
@@ -22,165 +22,72 @@
 
 #include <omp.h>
 
-#include <memory>
+#include <random>
 
 #include "reticolo/lattice/Lattice.hpp"
+#include "reticolo/modules/factory/MCAlgorithmBase.hpp"
 #include "reticolo/modules/montecarlo/MonteCarloData.hpp"
-#include "reticolo/modules/montecarlo/algorithms/AlgorithmBase.hpp"
 #include "reticolo/types/concepts.hpp"  // IWYU pragma: keep
 #include "reticolo/types/core.hpp"
 #include "reticolo/types/core_math.hpp"
 #include "reticolo/types/random.hpp"
 #include "yaml-cpp/node/node.h"
 
-namespace reticolo::montecarlo {
+namespace reticolo::MMonteCarlo {
 
 template <class Action>
-class Metropolis : public AlgorithmBase<Action> {
+class Metropolis : public MCAlgorithmBase<Action> {
   private:
     /* Types definitions */
-    using ActionType = typename Action::ActionType;
-    using FieldType = typename Action::FieldType;
-    using LatticeType = Lattice<typename Action::FieldType>;
-    using monte_carlo_data_type = montecarlo::data<typename Action::ActionType>;
+    using action_type = typename Action::ActionType;
+    using field_type = typename Action::FieldType;
+    using lattice_type = Lattice<field_type>;
+    using monte_carlo_data_type = MMonteCarlo::data<action_type>;
 
     /* HMC settings */
-    double _ProposalWidth;
+    RealD _ProposalWidth;
+
+    /* Distributions */
+    std::uniform_real_distribution<RealD> _Unif;  // Uniform distribution [0.0, 1.0]
+    std::normal_distribution<RealD>       _Norm;  // Normal distibution (mean: 0.0, stddev: 1.0)
 
   public:
     Metropolis() = default;
 
     /* setup */
-    void setup(const YAML::Node& HMCParams, const Lattice<FieldType>& Field) override {
-        _ProposalWidth = HMCParams["prop_width"].as<double>();
+    void setup(const YAML::Node& Params, const Lattice<field_type>& Field) override {
+        /* Parse parameters */
+        _ProposalWidth = Params["prop_width"].as<RealD>();
+
+        /* Initialize distributions */
+        _Unif = std::uniform_real_distribution<RealD>(0.0, 1.0);
+        _Norm = std::normal_distribution<RealD>(0.0, 1.0);
     }
 
     /* execution */
-    void updateField(Lattice<FieldType>& field, Action& action, monte_carlo_data_type& state) override {
-        uint       Acc = 0;       // acceptance
-        ActionType SVarTot(0.0);  // cumulative action variation
+    void updateField(Lattice<field_type>& field, Action& action, monte_carlo_data_type& state, RNGType& rng) override {
+        uint        Acc = 0;       // acceptance
+        action_type SVarTot(0.0);  // cumulative action variation
 
         // Loop over the entire lattice
         for (int Site = 0; Site < field.getNsites(); Site++) {
             // uint ThId = omp_get_thread_num();
             // Generate a randomized local field variation
-            FieldType FieldVar;  // local field variation
-            // randomize(FieldVar, _ProposalWidth, _UnifC, _Rng);
+            field_type FieldVar;  // local field variation
+            randomize(FieldVar, _ProposalWidth, _Norm, rng);
 
             // Compute the associated action variation
-            ActionType SVar = action.compute_dS_loc(field, FieldVar, Site);
+            action_type SVar = action.compute_dS_loc(field, FieldVar, Site);
 
             // Metropolis acceptance check
-            // if (exp(-make_real(SVar)) > m_Unif(_Rng)) {
-            //     Acc++;
-            //     field[Site] += FieldVar;
-            //     SVarTot += SVar;
-            // }
+            if (exp(-make_real(SVar)) > _Unif(rng)) {
+                Acc++;
+                field[Site] += FieldVar;
+                SVarTot += SVar;
+            }
         }
-        state.update(((double)Acc) / field.getNsites(), SVarTot);
+        state.update(((RealD)Acc) / field.getNsites(), SVarTot);
     }
 };
 
-}  // namespace reticolo::montecarlo
-
-// #pragma once
-
-// #include <string>
-
-// #include "reticolo/lattice/Lattice.hpp"
-// #include "reticolo/montecarlo/MonteCarloHandler.hpp"
-// #include "reticolo/tools/io_utils.hpp"
-// #include "reticolo/types/core.hpp"
-// #include "reticolo/types/core_math.hpp"
-
-// namespace reticolo::montecarlo {
-
-// template <class T>
-// concept MetropolisCapable = T::IsMetropolisCapable;
-
-// template <MetropolisCapable Action>
-// class Metropolis : public MonteCarloHandler<Action> {
-//   private:
-//     /* Types definitions */
-//     using ActionType = typename Action::ActionType;
-//     using FieldType = typename Action::FieldType;
-//     using LatticeType = Lattice<typename Action::FieldType>;
-
-//     /* Introduced symbols from MonteCarloHandler (avoids using ...) */
-//     using MonteCarloHandler<Action>::_Field;
-//     using MonteCarloHandler<Action>::_Action;
-//     using MonteCarloHandler<Action>::_McStats;
-//     using MonteCarloHandler<Action>::_Unif;
-//     using MonteCarloHandler<Action>::_UnifC;
-//     using MonteCarloHandler<Action>::_Norm;
-//     using MonteCarloHandler<Action>::_Rng;
-//     using MonteCarloHandler<Action>::_Logger;
-//     using MonteCarloHandler<Action>::_Timer;
-
-//     /* Metropolis settings */
-//     double _ProposalWidth = 1.0;
-
-//     /* override virtual updateField() method */
-//     void updateField() override;
-
-//   public:
-//     /* Constructor */
-//     Metropolis(std::string handler_name, Action& action, LatticeType& field, uint seed, const std::string&
-//     output_path,
-//                bool StdOut, bool save_data, bool save_config);
-
-//     /* Initialize parameters */
-//     void setParams(double propWidth) { _ProposalWidth = propWidth; }
-// };
-
-// template <MetropolisCapable Action>
-// Metropolis<Action>::Metropolis(std::string        handler_name,  //
-//                                Action&            action,        //
-//                                LatticeType&       field,         //
-//                                uint               seed,          //
-//                                const std::string& output_path,   //
-//                                bool               StdOut,        //
-//                                bool               save_data,     //
-//                                bool               save_config)
-//     : montecarlo::MonteCarloHandler<Action>(output_path,    //
-//                                             handler_name,   //
-//                                             action, field,  //
-//                                             seed,           //
-//                                             StdOut,         //
-//                                             save_data,      //
-//                                             save_config) {
-//     // Log stuff
-//     this->m_Logger << IO::LI_time() + std::format("MetropolisWorker - Initialization completed in {:.3f} ms\n",
-//                                                   this->_Timer.elapsed_ms());
-// }
-
-// template <MetropolisCapable Action>
-// void Metropolis<Action>::updateField() {
-//     uint       Acc = 0;       // acceptance
-//     ActionType SVarTot(0.0);  // cumulative action variation
-
-//     // Loop over the entire lattice
-//     for (int Site = 0; Site < _Field.getNsites(); Site++) {
-//         // uint ThId = omp_get_thread_num();
-//         // Generate a randomized local field variation
-//         FieldType FieldVar;  // local field variation
-//         randomize(FieldVar, _ProposalWidth, _UnifC, _Rng);
-
-//         // Compute the associated action variation
-//         ActionType SVar = _Action.compute_dS_loc(_Field, FieldVar, Site);
-
-//         // Metropolis acceptance check
-//         if (exp(-make_real(SVar)) > m_Unif(_Rng)) {
-//             Acc++;
-//             _Field[Site] += FieldVar;
-//             SVarTot += SVar;
-//         }
-//     }
-//     _McStats.update(((double)Acc) / _Field.getNsites(), SVarTot);
-// }
-
-// /* Argument deduction guide */
-// template <MetropolisCapable Action>
-// Metropolis(std::string, Action&, Lattice<typename Action::FieldType>, uint, std::string&, bool, bool,
-//            bool) -> Metropolis<Action>;
-// }  // namespace reticolo::montecarlo
+}  // namespace reticolo::MMonteCarlo
