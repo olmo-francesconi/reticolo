e5e06a44cb6306fcf880f1107e6827b29a2e145f
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-09 14:52:54 +0100

    added checks for not found method or action

--- src/reticolo/modules/factory/ModuleFactory.hpp
+++ src/reticolo/modules/factory/ModuleFactory.hpp
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
 
  - reticolo (www.github.com/olmo-francesconi/reticolo.git)
 
@@ -6,19 +6,20 @@
 
  - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
 
- ******************************************************************************/
+*******************************************************************************/
 
 #pragma once
 
 #include <cstddef>
+#include <format>
 #include <map>
 #include <memory>
+#include <stdexcept>
 #include <string>
 
 #include "reticolo/action/RelativisticBoseGas.hpp"
 #include "reticolo/modules/factory/ModuleBase.hpp"
 #include "reticolo/modules/montecarlo/MonteCarloHandler.hpp"
-#include "reticolo/types/core.hpp"
 
 namespace reticolo {
 
@@ -28,23 +29,37 @@ enum class Modules {
 std::map<std::string, Modules> ModMap{{"MonteCarlo", Modules::MonteCarlo}};
 
 enum class Actions {
-    RelativisticBoseGas,
+    RelativisticBoseGasF,
+    RelativisticBoseGasD,
     WeakFieldEuclideanGR,
 };
-std::map<std::string, Actions> ActMap{{"RelativisticBoseGas", Actions::RelativisticBoseGas},
+std::map<std::string, Actions> ActMap{{"RelativisticBoseGasF", Actions::RelativisticBoseGasF},
+                                      {"RelativisticBoseGasD", Actions::RelativisticBoseGasD},
                                       {"WeakFieldEuclideanGR", Actions::WeakFieldEuclideanGR}};
 
 class ModuleFactory {
   public:
-    static auto MakeModule(const std::string& name, const std::string& action) -> std::unique_ptr<ModuleBase> {
-        auto Mod = ModMap.find(name);
+    static auto MakeModule(const std::string& module_name, const std::string& action) -> std::unique_ptr<ModuleBase> {
+        auto Mod = ModMap.find(module_name);
         auto Act = ActMap.find(action);
+
+        if (Mod == ModMap.end()) {
+            throw std::runtime_error(std::format("Requested Module ({}) not found", action));
+            return nullptr;
+        }
+
+        if (Act == ActMap.end()) {
+            throw std::runtime_error(std::format("Requested Action ({}) not found", action));
+            return nullptr;
+        }
 
         switch (Mod->second) {
             case Modules::MonteCarlo:
                 switch (Act->second) {
-                    case Actions::RelativisticBoseGas:
-                        return std::make_unique<MMonteCarlo::MonteCarloHandler<action::RelativisticBoseGas>>();
+                    case Actions::RelativisticBoseGasF:
+                        return std::make_unique<MMonteCarlo::MonteCarloHandler<action::RelativisticBoseGasF>>();
+                    case Actions::RelativisticBoseGasD:
+                        return std::make_unique<MMonteCarlo::MonteCarloHandler<action::RelativisticBoseGasD>>();
                     default:
                         return nullptr;
                 }
