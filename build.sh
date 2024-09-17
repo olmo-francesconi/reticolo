892308d115c75f48ad787701af6e633eda3df9c7
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-17 12:19:14 +0100

    build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+cmake \
+    -S . \
+    -B ./build \
+    -DCMAKE_BUILD_TYPE="Release"  \
+    -DCMAKE_INSTALL_PREFIX="/opt/reticolo" \
+    -DCMAKE_CXX_COMPILER="/opt/homebrew/opt/llvm/bin/clang++" \
+    -DHDF5_ROOT="/opt/hdf5/llvm/c-threadsafe"
