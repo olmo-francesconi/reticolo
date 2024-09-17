6e10466e2760223a9b230ac057c81038cde9c94f
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-17 13:29:04 +0100

    back to header only

--- src/reticolo/lattice/Indexing.hpp
+++ src/reticolo/lattice/Indexing.hpp
@@ -2,7 +2,7 @@
 
  - reticolo (www.github.com/olmo-francesconi/reticolo.git)
 
- - SourceFile: lattice/Indexing.hpp
+ - SourceFile: lattice/indexing.hpp
 
  - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
 
@@ -10,7 +10,11 @@
 
 #pragma once
 
+#include <functional>
+#include <numeric>
 #include <vector>
+
+#include "reticolo/types/core_math.hpp"
 
 namespace reticolo {
 
@@ -35,6 +39,41 @@ class Indexing {
     /* Get next/prev indexes */
     auto nextId(size_type site, size_type dir) -> size_type { return Next[site * Dims + dir]; }
     auto prevId(size_type site, size_type dir) -> size_type { return Prev[site * Dims + dir]; }
+};
+
+/* Constructor */
+Indexing::Indexing(const std::vector<size_type>& shape)
+    : Sizes(shape),
+      SubVols(shape.size()),
+      NSites(std::accumulate(Sizes.begin(), Sizes.end(), 1, std::multiplies<>())),
+      Dims(shape.size()) {
+    /* Fil the sub-volume vector */
+    for (size_type SubDim = 0; SubDim < Dims - 1; SubDim++) {
+        SubVols[SubDim] = std::accumulate(Sizes.begin() + SubDim + 1, Sizes.end(), 1, std::multiplies<>());
+    }
+    SubVols.back() = 1;
+
+    /* support variables to keep track of position in the lattice */
+    std::vector<size_type> Coord(Dims, 0);
+    std::vector<size_type> PrevCoord(Dims);
+    std::vector<size_type> NextCoord(Dims);
+
+    /* Resize the vectors of neighbours */
+    Next.resize(NSites * Dims, 0);
+    Prev.resize(NSites * Dims, 0);
+
+    /* Loop throught the lattice to generate all the closest neighbours */
+    for (size_type Site = 0; Site < NSites; Site++) {
+        for (size_type Dir = 0; Dir < Dims; Dir++) {
+            NextCoord = Coord;
+            NextCoord[Dir] = (Coord[Dir] + 1) % Sizes[Dir];
+            Next[Site * Dims + Dir] = dot(NextCoord, SubVols);
+            PrevCoord = Coord;
+            PrevCoord[Dir] = (Coord[Dir] + (Sizes[Dir] - 1)) % Sizes[Dir];
+            Prev[Site * Dims + Dir] = dot(PrevCoord, SubVols);
+        }
+        advance_coord(Sizes, Coord);
+    }
 };
 
 }  // namespace reticolo
--- src/reticolo/lattice/lattice.hpp
+++ src/reticolo/lattice/lattice.hpp
@@ -12,12 +12,16 @@
 
 #include <H5public.h>
 
+#include <algorithm>
 #include <concepts>
 #include <cstdlib>
+#include <iostream>
 #include <memory>
+#include <string>
 #include <vector>
 
-#include "reticolo/lattice/Indexing.hpp"
+#include "reticolo/lattice/indexing.hpp"
+#include "reticolo/tools/io_utils.hpp"
 #include "reticolo/types/concepts.hpp"  // IWYU pragma: keep
 #include "reticolo/types/core.hpp"
 
@@ -32,20 +36,49 @@ class Lattice : public std::vector<TField> {
     /* Public member variables */
     std::shared_ptr<Indexing> Idx;
 
-    /* Constructors */
-    Lattice();                                                 // default
-    Lattice(const std::vector<size_type>& shape);              // shape constructor
-    Lattice(const std::vector<size_type>& shape, TField val);  // value-initialized
-    Lattice(const Lattice& obj);                               // copy constructor
-    template <typename T>                                      //
-        requires(!std::same_as<TField, T>)                     // copy constructor from latttie of different type
-    Lattice(const Lattice<T>& obj);                            //
+    /* Default constructor */
+    Lattice() = default;
+    /* Shape constructor */
+    Lattice(const std::vector<size_type>& shape) {
+        Idx = std::make_shared<Indexing>(shape);
+        this->resize(Idx->NSites);
+    };
+    /* Value-initialized contructor */
+    Lattice(const std::vector<size_type>& shape, TField val) {
+        Idx = std::make_shared<Indexing>(shape);
+        this->resize(Idx->NSites, val);
+    };
+    /* Copy constructor */
+    Lattice(const Lattice& obj) {
+        Idx = obj.Idx;
+        this->insert(this->begin(), obj.begin(), obj.end());
+    };
+    /* Copy constructor (templated) */
+    template <typename T>
+        requires(!std::same_as<TField, T>)
+    Lattice(const Lattice<T>& obj) {
+        Idx = obj.Idx;
+        this->resize(Idx->NSites);
+    };
 
     /* Destructor */
-    ~Lattice();
+    ~Lattice() = default;
 
     /* Copy assignment */
-    auto operator=(const Lattice& other) -> Lattice&;
+    auto operator=(const Lattice& other) -> Lattice& {
+        // Guard self assignment
+        if (this == &other) {
+            return *this;
+        }
+        // Copy only if compatible
+        if (this->getSizes() == other.getSizes()) {
+            std::copy(other.begin(), other.end(), this->begin());
+        } else {
+            std::cerr << IO::LI_erro() + "reticolo::Lattice - Copy Assigment failed [incompatible lattice sizes]\n";
+            exit(EXIT_FAILURE);
+        }
+        return *this;
+    }
 
     /* Getters for lattice parameters */
     [[nodiscard]] auto getNt() const -> size_type { return Idx->Sizes[_t]; }
