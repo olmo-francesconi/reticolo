122245e9d55436b98abab0e4e5dcf7d0a9d3abfd
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-20 14:38:37 +0100

    commented unused stuff

--- src/reticolo/core/tools/memoryReport.hpp
+++ src/reticolo/core/tools/memoryReport.hpp
@@ -1,85 +1,85 @@
-/******************************************************************************
+// /******************************************************************************
 
- - reticolo (www.github.com/olmo-francesconi/reticolo.git)
+//  - reticolo (www.github.com/olmo-francesconi/reticolo.git)
 
- - SourceFile: tools/io_utils.hpp
+//  - SourceFile: tools/io_utils.hpp
 
- - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
+//  - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
 
-******************************************************************************/
+// ******************************************************************************/
 
-#pragma once
+// #pragma once
 
-#include <cstddef>
-#include <format>
-#include <map>
-#include <string>
-#include <utility>
-#include <vector>
+// #include <cstddef>
+// #include <format>
+// #include <map>
+// #include <string>
+// #include <utility>
+// #include <vector>
 
-#include "reticolo/tools/io_utils.hpp"
-#include "reticolo/types/core.hpp"
+// #include "reticolo/tools/io_utils.hpp"
+// #include "reticolo/types/core.hpp"
 
-namespace reticolo {
+// namespace reticolo {
 
-class MemoryReportLayer {
-  private:
-    MemoryReportLayer*              m_Parent;
-    std::vector<MemoryReportLayer*> m_Daughters;
-    std::string                     m_Name;
-    uint                            m_Depth{0};
-    size_t                          m_Tot{0};
-    std::map<std::string, size_t>   m_LayerReport;
+// class MemoryReportLayer {
+//   private:
+//     MemoryReportLayer*              m_Parent;
+//     std::vector<MemoryReportLayer*> m_Daughters;
+//     std::string                     m_Name;
+//     uint                            m_Depth{0};
+//     size_t                          m_Tot{0};
+//     std::map<std::string, size_t>   m_LayerReport;
 
-  public:
-    explicit MemoryReportLayer(std::string LayerName, MemoryReportLayer* Parent = nullptr)
-        : m_Parent(Parent), m_Name(std::move(LayerName)) {}
-    auto getName() -> std::string { return m_Name; }
-    auto getParent() -> MemoryReportLayer* { return m_Parent; }
-    void increaseDepth() {
-        m_Depth++;
-        for (auto* const Daughter : m_Daughters) {
-            Daughter->increaseDepth();
-        }
-    }
+//   public:
+//     explicit MemoryReportLayer(std::string LayerName, MemoryReportLayer* Parent = nullptr)
+//         : m_Parent(Parent), m_Name(std::move(LayerName)) {}
+//     auto getName() -> std::string { return m_Name; }
+//     auto getParent() -> MemoryReportLayer* { return m_Parent; }
+//     void increaseDepth() {
+//         m_Depth++;
+//         for (auto* const Daughter : m_Daughters) {
+//             Daughter->increaseDepth();
+//         }
+//     }
 
-    [[nodiscard]] auto getDepth() const -> int { return m_Depth; }
-    [[nodiscard]] auto getTot() const -> size_t { return m_Tot; }
+//     [[nodiscard]] auto getDepth() const -> int { return m_Depth; }
+//     [[nodiscard]] auto getTot() const -> size_t { return m_Tot; }
 
-    void addDaughter(MemoryReportLayer* NewDaughter) {
-        m_Daughters.push_back(NewDaughter);
-        m_Tot += NewDaughter->getTot();
-        NewDaughter->increaseDepth();
-    }
-    void addEntry(const std::string& key, size_t value) {
-        m_LayerReport.insert({key, value});
-        m_Tot += value;
-    }
+//     void addDaughter(MemoryReportLayer* NewDaughter) {
+//         m_Daughters.push_back(NewDaughter);
+//         m_Tot += NewDaughter->getTot();
+//         NewDaughter->increaseDepth();
+//     }
+//     void addEntry(const std::string& key, size_t value) {
+//         m_LayerReport.insert({key, value});
+//         m_Tot += value;
+//     }
 
-    auto getFullReport() -> std::string {
-        std::string Res;
-        Res += std::format("{:.<48}[{}]\n", std::string(2 * m_Depth, ' ') + m_Name, IO::pretty_bytes(m_Tot));
-        for (const auto& [key, value] : m_LayerReport) {
-            Res += std::format("{:<48}[{}]\n", std::string(2 * (m_Depth + 1), ' ') + key, IO::pretty_bytes(value));
-        }
-        for (auto* const Daughter : m_Daughters) {
-            Res += Daughter->getFullReport();
-        }
-        return Res;
-    }
+//     auto getFullReport() -> std::string {
+//         std::string Res;
+//         Res += std::format("{:.<48}[{}]\n", std::string(2 * m_Depth, ' ') + m_Name, IO::pretty_bytes(m_Tot));
+//         for (const auto& [key, value] : m_LayerReport) {
+//             Res += std::format("{:<48}[{}]\n", std::string(2 * (m_Depth + 1), ' ') + key, IO::pretty_bytes(value));
+//         }
+//         for (auto* const Daughter : m_Daughters) {
+//             Res += Daughter->getFullReport();
+//         }
+//         return Res;
+//     }
 
-    auto getShortReport() -> std::string {
-        std::string Res;
-        Res += std::format("{:.<48}[{}]\n", std::string(2 * m_Depth, ' ') + m_Name, IO::pretty_bytes(m_Tot));
-        size_t OtherTot = 0;
-        for (const auto& [_, value] : m_LayerReport) {
-            OtherTot += value;
-        }
-        Res += std::format("{:<48}[{}]\n", std::string(2 * (m_Depth + 1), ' ') + "Others", IO::pretty_bytes(OtherTot));
-        for (auto* const Daughter : m_Daughters) {
-            Res += Daughter->getShortReport();
-        }
-        return Res;
-    }
-};
-}  // namespace reticolo
+//     auto getShortReport() -> std::string {
+//         std::string Res;
+//         Res += std::format("{:.<48}[{}]\n", std::string(2 * m_Depth, ' ') + m_Name, IO::pretty_bytes(m_Tot));
+//         size_t OtherTot = 0;
+//         for (const auto& [_, value] : m_LayerReport) {
+//             OtherTot += value;
+//         }
+//         Res += std::format("{:<48}[{}]\n", std::string(2 * (m_Depth + 1), ' ') + "Others",
+//         IO::pretty_bytes(OtherTot)); for (auto* const Daughter : m_Daughters) {
+//             Res += Daughter->getShortReport();
+//         }
+//         return Res;
+//     }
+// };
+// }  // namespace reticolo
--- src/reticolo/core/tools/signalHandler.hpp
+++ src/reticolo/core/tools/signalHandler.hpp
@@ -1,42 +1,42 @@
-/******************************************************************************
+// /******************************************************************************
 
- - reticolo (www.github.com/olmo-francesconi/reticolo.git)
+//  - reticolo (www.github.com/olmo-francesconi/reticolo.git)
 
- - SourceFile: tools/sig_handler.hpp
+//  - SourceFile: tools/sig_handler.hpp
 
- - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
+//  - Author: Olmo Francesconi <olmo.francesconi@glasgow.ac.uk>
 
-******************************************************************************/
-#pragma once
+// ******************************************************************************/
+// #pragma once
 
-#include <csignal>
-#include <cstdlib>
-#include <iostream>
+// #include <csignal>
+// #include <cstdlib>
+// #include <iostream>
 
-#include "reticolo/tools/io_utils.hpp"
+// #include "reticolo/tools/io_utils.hpp"
 
-using Handler = void (*)(int);
+// using Handler = void (*)(int);
 
-namespace reticolo::SignalHandler {
+// namespace reticolo::SignalHandler {
 
-/* SIGINT soft-exit handler */
-inline bool SoftExitRequested = false;
-inline void SIGINT_SoftExit(int signum) {
-    if (!SoftExitRequested) {
-        std::cout << '\n' << IO::LI_warn() << "SoftExit requested (Ctrl+C again to exit immediately)\n";
-    } else {
-        std::cout << '\n' << IO::LI_erro() << "Immediate exit requested, possible data corruption.\n";
-        std::cout << IO::LI_erro() << "Bye :(\n";
+// /* SIGINT soft-exit handler */
+// inline bool SoftExitRequested = false;
+// inline void SIGINT_SoftExit(int signum) {
+//     if (!SoftExitRequested) {
+//         std::cout << '\n' << IO::LI_warn() << "SoftExit requested (Ctrl+C again to exit immediately)\n";
+//     } else {
+//         std::cout << '\n' << IO::LI_erro() << "Immediate exit requested, possible data corruption.\n";
+//         std::cout << IO::LI_erro() << "Bye :(\n";
 
-        exit(SIGINT);
-    }
-    SoftExitRequested = true;
-}
+//         exit(SIGINT);
+//     }
+//     SoftExitRequested = true;
+// }
 
-/* Install new handler */
-inline void set_SIGINT_handler(void (*new_hadler)(int)) { signal(SIGINT, new_hadler); }
+// /* Install new handler */
+// inline void set_SIGINT_handler(void (*new_hadler)(int)) { signal(SIGINT, new_hadler); }
 
-/* Restore original handler */
-inline void SIGINT_reset() { signal(SIGINT, SIG_DFL); }
+// /* Restore original handler */
+// inline void SIGINT_reset() { signal(SIGINT, SIG_DFL); }
 
-}  // namespace reticolo::SignalHandler
+// }  // namespace reticolo::SignalHandler
