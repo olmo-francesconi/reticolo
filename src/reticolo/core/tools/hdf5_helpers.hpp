1cb8446060e582c16d423a0b0b082eb36d9c56a8
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-20 14:37:12 +0100

    added tempalte specialization for uint types

--- src/reticolo/core/tools/hdf5_helpers.hpp
+++ src/reticolo/core/tools/hdf5_helpers.hpp
@@ -1,10 +1,32 @@
 #pragma once
 
 #include <H5Ipublic.h>
+#include <H5Tpublic.h>
+#include <omp.h>
+
+#include <cstddef>
 
 namespace reticolo {
 
+/*--------------------------------------------------------------------------------------------------
+    Hdf5 type make template function
+--------------------------------------------------------------------------------------------------*/
 template <typename T>
 inline auto make_H5_Type() -> hid_t;
 
+template <>
+inline auto make_H5_Type<unsigned short>() -> hid_t {
+    return H5T_NATIVE_USHORT;
 }
+
+template <>
+inline auto make_H5_Type<unsigned int>() -> hid_t {
+    return H5T_NATIVE_UINT;
+}
+
+template <>
+inline auto make_H5_Type<size_t>() -> hid_t {
+    return H5T_NATIVE_ULONG;
+}
+
+}  // namespace reticolo
