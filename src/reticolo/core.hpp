64ca6c3c820a4c02d062cfafa3aeef4f3ba1f0ac
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-09-23 11:21:30 +0100

    added includes

--- src/reticolo/core.hpp
+++ src/reticolo/core.hpp
@@ -11,7 +11,19 @@
 #pragma once
 
 /* types */
+#include "reticolo/core/types/SO(N).hpp"    // IWYU pragma: keep
+#include "reticolo/core/types/SU(N).hpp"    // IWYU pragma: keep
 #include "reticolo/core/types/complex.hpp"  // IWYU pragma: keep
+#include "reticolo/core/types/coord.hpp"    // IWYU pragma: keep
+#include "reticolo/core/types/hfield.hpp"   // IWYU pragma: keep
 #include "reticolo/core/types/real.hpp"     // IWYU pragma: keep
 
 /* tools */
+#include "reticolo/core/tools/Hdf5Handler.hpp"   // IWYU pragma: keep
+#include "reticolo/core/tools/hdf5_helpers.hpp"  // IWYU pragma: keep
+#include "reticolo/core/tools/io_utils.hpp"      // IWYU pragma: keep
+#include "reticolo/core/tools/logger.hpp"        // IWYU pragma: keep
+#include "reticolo/core/tools/timer.hpp"         // IWYU pragma: keep
+
+/* physical constants */
+#include "reticolo/core/physics/constants.hpp"  // IWYU pragma: keep
