9d5d167dab575b8570cffb5d678c8122b4cff340
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-10-17 13:39:23 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ cmake \
     -DCMAKE_INSTALL_PREFIX="/opt/reticolo" \
     -DCMAKE_CXX_COMPILER="/opt/homebrew/opt/llvm/bin/clang++" \
     -DHDF5_ROOT="/opt/hdf5/llvm/c-threadsafe"
-# -DSMALL_LATTICE=ON
