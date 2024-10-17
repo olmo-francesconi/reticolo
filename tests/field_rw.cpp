0d71539d5ad543a638319f07b5d5c0de1ebf9b41
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-10-17 10:47:43 +0100

    rw to second lattice and rng check

--- tests/field_rw.cpp
+++ tests/field_rw.cpp
@@ -12,17 +12,13 @@
 
 #include <cstddef>
 #include <cstdlib>
-#include <format>
 #include <iostream>
-#include <ostream>
 #include <random>
 #include <sstream>
 #include <string>
 #include <vector>
 
 #include "reticolo/core/tools/Hdf5Handler.hpp"
-#include "reticolo/core/tools/timer.hpp"
-#include "reticolo/core/types/complex.hpp"
 #include "reticolo/core/types/real.hpp"
 #include "reticolo/lattice/indexing.hpp"
 #include "reticolo/lattice/lattice.hpp"
@@ -33,14 +29,15 @@ using i_t = Indexing::size_type;
 
 auto main(int argc, char* argv[]) -> int {
     const std::vector<i_t> Size({4, 4, 4, 4});
-    Lattice<RealD>         Field(Size);
+    Lattice<RealD>         FieldOut(Size);
+    Lattice<RealD>         FieldIn(FieldOut);
     std::cout << "Lattice object initialized\n\n";
 
     std::cout << "Randomizing the lattice.. ";
     std::mt19937_64                 Rng;
     std::normal_distribution<RealD> Norm(0.0, 1.0);
     Rng.seed(0);
-    for (auto& Site : Field) {
+    for (auto& Site : FieldOut) {
         randomize(Site, 1.0, Norm, Rng);
     }
     std::cout << "done\n\n";
@@ -48,15 +45,18 @@ auto main(int argc, char* argv[]) -> int {
     std::cout << "Saving the field to out.hdf5.. ";
     std::stringstream RngState;
     RngState << Rng;
-    GlobalHdf5Handler.saveLattice("./out.hdf5", "rw_test", Field, RngState);
+    std::cout << Rng;
+    GlobalHdf5Handler.saveLattice("./out.hdf5", "rw_test", FieldOut, RngState);
     std::cout << "done\n\n";
 
     std::cout << "Reading in the file.. ";
-    GlobalHdf5Handler.readLattice("./out.hdf5", "rw_test", Field, RngState);
+    GlobalHdf5Handler.readLattice("./out.hdf5", "rw_test", FieldIn, RngState);
+    std::cout << Rng;
     std::cout << "done\n\n";
 
     std::cout << "Saving the field to out2.hdf5.. ";
-    RngState << Rng;
-    GlobalHdf5Handler.saveLattice("./out2.hdf5", "rw_test", Field, RngState);
+    GlobalHdf5Handler.saveLattice("./out2.hdf5", "rw_test", FieldOut, RngState);
+    std::cout << Rng;
+
     std::cout << "done\n\n";
 }
