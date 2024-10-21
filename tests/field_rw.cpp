fc6198c48baf7f8ea4ee54034dbc65bc628815f8
Olmo Francesconi <olmo.francesconi@gmail.com>
2024-10-21 11:57:58 +0100

    updated config read test

--- tests/field_rw.cpp
+++ tests/field_rw.cpp
@@ -12,13 +12,16 @@
 
 #include <cstddef>
 #include <cstdlib>
+#include <exception>
 #include <iostream>
+#include <numeric>
 #include <random>
 #include <sstream>
 #include <string>
 #include <vector>
 
 #include "reticolo/core/tools/Hdf5Handler.hpp"
+#include "reticolo/core/types/complex.hpp"
 #include "reticolo/core/types/real.hpp"
 #include "reticolo/lattice/indexing.hpp"
 #include "reticolo/lattice/lattice.hpp"
@@ -28,9 +31,10 @@ using namespace reticolo;
 using i_t = Indexing::size_type;
 
 auto main(int argc, char* argv[]) -> int {
-    const std::vector<i_t> Size({64, 64, 64, 64});
-    Lattice<RealD>         FieldOut(Size);
-    Lattice<RealD>         FieldIn(FieldOut);
+    const std::vector<i_t> SizeOut({4, 4, 4, 4});
+    Lattice<ComplexD>      FieldOut(SizeOut);
+    const std::vector<i_t> SizeIn({4, 4, 4, 4});
+    Lattice<ComplexD>      FieldIn(SizeIn);
     std::cout << "Lattice object initialized\n\n";
 
     std::cout << "Randomizing the lattice.. ";
@@ -47,13 +51,22 @@ auto main(int argc, char* argv[]) -> int {
     RngState << Rng;
     GlobalHdf5Handler.saveLattice("./out.hdf5", "rw_test", FieldOut, RngState);
     std::cout << "done\n";
+    std::cout << "Field sum: " << std::reduce(FieldOut.begin(), FieldOut.end()) << "\n";
     std::cout << "Next randon munber: " << Rng() << "\n\n";
 
     std::cout << "Reading in the file.. ";
-    GlobalHdf5Handler.readLattice("./out.hdf5", "rw_test", FieldIn, RngState);
+    try {
+        GlobalHdf5Handler.readLattice("./out.hdf5", "rw_test", FieldIn, RngState);
+    } catch (std::exception& e) {
+        std::cout << "\n" << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
     std::cout << "done\n";
+    std::cout << "Field sum: " << std::reduce(FieldIn.begin(), FieldIn.end()) << "\n";
     RngState >> Rng;
     std::cout << "Next randon munber: " << Rng() << "\n\n";
 
-    std::cout << "done\n\n";
+    std::cout << "All done!\n";
+
+    return EXIT_SUCCESS;
 }
